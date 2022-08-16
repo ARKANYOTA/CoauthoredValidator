8bc51844b635bad360a18487d60d8ba665875e74
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2022-08-16 22:41:16 +0200

    OK

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ CO_AUTHOR       = os.getenv('CO_AUTHOR')
 CO_AUTHOR_EMAIL = os.getenv('CO_AUTHOR_EMAIL')
 
 def main():
-    os.system("git checkout -b \"newbranch\"")
+    os.system("git checkout -b newbranch")
     for it in range(TIER):
         TextData = f'Iteration n°{it} of {TIER} \n\nCo-authored-by: {CO_AUTHOR} <{CO_AUTHOR_EMAIL}>'
         os.system(f'git commit --allow-empty -m "{TextData}"')	
-    os.system('git push')
-    os.system("git switch \"main\"")
-    os.system("git branch -delete \'newbranch\'")
+    os.system('git push --set-upstream origin newbranch')
+    os.system("git switch main")
+    os.system("git branch --delete newbranch")
 
 
 if __name__ == "__main__":
