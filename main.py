402a7e728757f86ef6eb7d37eeec12e283d392ff
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2022-08-16 22:56:15 +0200

    OK

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         os.system(f"git checkout -b newbranch{it}of{TIER}")
         TextData = f'Iteration n°{it} of {TIER} \n\nCo-authored-by: {CO_AUTHOR} <{CO_AUTHOR_EMAIL}>'
         os.system(f'git commit --allow-empty -m "{TextData}"')	
-        os.system('git push --set-upstream origin {it}of{TIER}')
+        os.system(f'git push --set-upstream origin {it}of{TIER}')
         os.system("git switch main")
 
 
